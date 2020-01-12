813ad45402a1edf499edf1f8b9b3b0fca384769b
modeinfo <radhwen.hamdani@gmail.com>
2020-01-12 17:17:17 +0100

    sub task add

--- form.js
+++ form.js
@@ -33,26 +33,32 @@ function checkIfEmpty(form){
         case 0:
           document.getElementById('first_name_check').setAttribute('style', 'display: none;');
           document.getElementById('first_name').setAttribute('style', '');
+          var first_name_ = true;
         break;
         case 1:
           document.getElementById('last_name_check').setAttribute('style', 'display: none;');
           document.getElementById('last_name').setAttribute('style', '');
+          var last_name_ = true;
         break;
         case 2:
           document.getElementById('email_check').setAttribute('style', 'display: none;');
           document.getElementById('email').setAttribute('style', '');
+          var email_ = true;
         break;
         case 3:
           document.getElementById('bio_check').setAttribute('style', 'display: none;');
           document.getElementById('bio').setAttribute('style', '');
+          var bio_ = true;
         break;
         case 4:
           document.getElementById('tel_number_check').setAttribute('style', 'display: none;');
           document.getElementById('tel_number').setAttribute('style', '');
+          var tel_number_ = true;
         break;
         case 5:
           document.getElementById('address_check').setAttribute('style', 'display: none;');
           document.getElementById('address').setAttribute('style', '');
+          var address_ = true;
         break;
       }
     }
@@ -68,6 +74,7 @@ function checkEmailFormat(email){
   }else{
     document.getElementById('email_check_format').setAttribute('style', 'display: none;');
     document.getElementById('email').setAttribute('style', '');
+    var format_email_ = true;
   }
 }
 
@@ -78,6 +85,7 @@ function checkPhoneNumber(tel_number){
   }else{
     document.getElementById('tel_number').setAttribute('style', '');
     document.getElementById('tel_number_check_max').setAttribute('style', 'display: none;');
+    var tel_number_length_ = true;
   }
   if(isNaN(tel_number)){
     document.getElementById('tel_number').setAttribute('style', 'border: 2px solid red;');
@@ -85,6 +93,7 @@ function checkPhoneNumber(tel_number){
   }else{
     document.getElementById('tel_number').setAttribute('style', '');
     document.getElementById('tel_number_check_char').setAttribute('style', 'display: none;');
+    var tel_number_number_ = true;
   }
 
 }
@@ -101,5 +110,38 @@ function validation(){
   
   checkIfEmpty(form);
   checkEmailFormat(email);
+
+  console.log(first_name_);
+  console.log(tel_number_number_);
+}
+
+function validationHobby(val){
+  var hobby = document.getElementById('hobby').value;
+
+  if(val == "add"){
+    if(hobby == undefined || hobby == ''){
+      document.getElementById('hobby_check').setAttribute('style', 'display: block;color: red;');
+      document.getElementById('hobby').setAttribute('style', 'border: 2px solid red;');
+    }else if(hobby != undefined && hobby != ''){
+      document.getElementById('hobby_check').setAttribute('style', 'display: none;');
+      document.getElementById('hobby').setAttribute('style', '');
+
+      var hobbyList = document.getElementById('hobbyList').innerHTML;
+
+      document.getElementById('hobbyList').innerHTML = hobbyList + "<li>" + hobby + "</li>";
+      document.getElementById('hobby').value = '';
+
+    }
+  }
+  
+  if(val == "submit"){
+    var x = document.getElementById("hobbyList").childElementCount;
+    if(x < 3){
+      document.getElementById('hobbiesArea_check').setAttribute('style', 'display: block;color: red;');
+    }else{
+      document.getElementById('hobbiesArea_check').setAttribute('style', 'display: none;');
+      document.getElementById('hobbiesArea_success').setAttribute('style', 'display: block;color: blue;');
+    }
+  }  
 
 }
