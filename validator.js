a6be66ea04b8e14afa0abc88c60e185a48add097
r.khiari <rania.rania.khiari@gmail.com>
2020-01-14 19:58:08 +0100

    SUB TASK

--- validator.js
+++ validator.js
@@ -8,6 +8,7 @@ var number = document.form.number.value;
 var address = document.form.address.value;
 var phone_number = /^\d{8}$/;
 
+
 if (user_name === "" || last_name === "" || mail ==="" || bio ==="" || number ==="" || address ==="")
 {
 alert("Please input a Value");
@@ -23,4 +24,28 @@ return false;
 }
 else
 return true;
+}
+
+function addHoppy()
+{
+    var textVal = document.getElementById('textVal').value,
+    listNode = document.getElementById('list'),
+    liNode = document.createElement("LI"),
+    txtNode = document.createTextNode(textVal);
+    if(textVal !== ""){
+        liNode.appendChild(txtNode);
+        listNode.appendChild(liNode);
+    }else{
+        alert('you should enter your hoppy');
+        }
+}
+
+function validationHoppy(){
+    var x = document.getElementById("list").getElementsByTagName("li").length;
+    if(x >= 3)
+        return true;
+    else{
+        alert("You must have at least three hobbies");
+        return false;
+    }
 }
